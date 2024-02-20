06ad48d9453852611e1879a7409b109f3b8d6111
jyotirmoy1997 <subhodas45036@gmail.com>
2024-02-20 19:05:45 +0530

    Refactored Listing Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,36 +1,11 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    class Listing{
-        public static function all(){
-            return [
-                [
-                    'id' => '1',
-                    'title' => 'SDE-1',
-                    'description' => 'Work in Backend',
-                    'salary' => '$80000'
-                ],
-                [
-                    'id' => '2',
-                    'title' => 'SDE-2',
-                    'description' => 'Work in Backend',
-                    'salary' => '$120000'
-                ]
-            ];
-        }
-        public static function find($id){
-            $listings = self::all();
-            foreach($listings as $listing){
-                if($listing['id'] === $id){
-                    return $listing;
-                }
-                    
-                 
-            }
-        }
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}
